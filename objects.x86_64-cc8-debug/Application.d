07a158716a94c7052d0febcdc3a5e4c87650a80d
Alex Hitech <ahitech@gmail.com>
2020-11-29 00:15:29 +0200

    Initial check-in

--- objects.x86_64-cc8-debug/Application.d
+++
@@ -1,5 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86_64-cc8-debug/Application.o : Application.h AppWindow.h \
-    MainView.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
--- objects.x86_64-cc8-debug/MainView.d
+++
@@ -1,4 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86_64-cc8-debug/MainView.o : MainView.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
