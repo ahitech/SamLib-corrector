1558f0da3e4376aaec1b4b36a8b342f062add879
Alex Hitech <ahitech@gmail.com>
2021-03-10 00:40:40 +0200

    Updates to SourceView

--- SourceView.cpp
+++ SourceView.cpp
@@ -12,26 +12,24 @@ SourceTextView::SourceTextView (BRect rect,
 								const char *name)
 	: BTextView(rect,
 				name,
-				frame.InsetBy(5, 5),
-				be_fixed_font,
-				(rgb_color){0, 0, 0, 255},
+				rect,
 				B_FOLLOW_LEFT | B_FOLLOW_TOP,
-				B_WILL_DRAW | B_PULSE_NEEDED | B_FRAME_EVENTS | B_NAVIGABLE);
+				B_WILL_DRAW | B_FULL_UPDATE_ON_RESIZE | B_NAVIGABLE)
 {
-	
+	return;
 	
 }
 
 bool SourceTextView::AcceptsDrop(BMessage *archive)
 {
-		
+	return true;		
 }
 
 
 
 bool SourceTextView::AcceptsPaste(BClipboard *clip)
 {
-	
+	return true;
 }
 
 
@@ -46,12 +44,12 @@ SourceView::SourceView (BRect rect,
 	if (textView)
 	{
 		scrollBars = new BScrollView("scroller",
-						            textView,
-						            B_FOLLOW_LEFT | B_FOLLOW_TOP,
-						            0,
-						            true,
-						            true,
-						            B_FANCY_BORDER)
+									textView,
+									B_FOLLOW_LEFT | B_FOLLOW_TOP,
+									0,
+									true,
+									true,
+									B_FANCY_BORDER);
 	}
 }
 
--- SourceView.h
+++ SourceView.h
@@ -7,22 +7,29 @@
 
 #include <SupportDefs.h>
 
+#include <Rect.h>
 #include <ScrollView.h>
 #include <TextView.h>
 
-
+/*!	\class		SourceTextView	SourceView.h
+ *	\brief		This class holds the data to be processed.
+ *	\details	Pasting from the text editor is performed into this View.
+ *				Therefore, it has to accept HTML and support drag-n-drop.
+ */
 class SourceTextView : public BTextView 
 {
 	public:
 		SourceTextView(BRect frame, const char *name);
-		~SourceTextView();
+		virtual ~SourceTextView() {};
 
 		// This BTextView accepts HTML
 		bool AcceptsDrop (BMessage *archive);
 		bool AcceptsPaste(BClipboard *clipboard);
-}
+};
 
-
+/*!	\class		SourceView	SourceView.h
+ *	\brief		SourceTextView with scrollbars.
+ */
 class SourceView
 {
 public:
